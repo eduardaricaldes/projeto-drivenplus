6ef19c2219d4784b823d4895f7f888cd99058c5a
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-10-30 18:32:20 -0300

    criado usuario e autenticacao provider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+
+import {AutenticacaoContext} from './contexts/AutenticacaoProvider'
+
 import Login from "./pages/Login";
 import SignUp from "./pages/Sign-up";
 import Planos from "./pages/Planos";
 import Assinatura from "./pages/Assinatura";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const RotaPrivada = ({ Componente }) => {
+  const [token, setToken, logado] = useContext(AutenticacaoContext)
 
+  return logado ? <Componente /> : <Login />
+}
 
 export default function App(){
   return(
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Login/>} path="/"/>
-        <Route element={<SignUp />} path="/signup"/>
-        <Route element={<Planos/>} path="/subscriptions" />
-        <Route element={<Assinatura/>} path="/subscriptions/id" />
-        <Route element={<Home />} path="/home"/>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route element={<Login/>} path="/"/>
+      <Route element={<SignUp />} path="/signup"/>
+      <Route element={<RotaPrivada Componente={<Planos/>}/>} path="/subscriptions" />
+      <Route element={<RotaPrivada Componente={<Assinatura/>}/>} path="/subscriptions/:id" />
+      <Route element={<RotaPrivada Componente={<Home />}/>} path="/home"/>
+    </Routes>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import * as ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import "./reset.css"
+
+import { AutenticacaoProvider } from './contexts/AutenticacaoProvider';
+import { UsuarioProvider } from './contexts/UserContext';
+
 import App from './App';
-ReactDom.render(<App />, document.getElementById('root'))
+
+ReactDom.render(
+<BrowserRouter>
+  <AutenticacaoProvider>
+    <UsuarioProvider>
+      <App />
+    </UsuarioProvider>
+  </AutenticacaoProvider>
+</BrowserRouter>
+, document.getElementById('root'));
